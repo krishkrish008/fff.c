d27710fda8b23e36a6111a2dad06d059f9039ec4
krishkrish008 <35235157+krishkrish008@users.noreply.github.com>
2018-01-21 16:20:39 +0530

    Create kk.c

---
+++ kk.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int n,N,sum;
+clrscr();
+printf("Enter value upto n number has to be calculated");
+scanf("%d",&n);
+N=n(n-1);
+sum=N/2;
+printf("sum of natural number is %d",sum);
+return 0;
